26cfd931d73b29ee9e1df738af82ff32fb36f849
jinyoungKim2021182008 <wlsdudkim9693@gmail.com>
2022-10-01 22:03:07 +0900

    캐릭터 8방향 이동

--- character.py
+++ character.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import math
 
 CHARACTER_WIDTH, CHARACTER_HEIGHT = 50, 50
 STAGE_WIDTH, STAGE_LENGTH = 800, 800
@@ -6,11 +7,16 @@ STAGE_WIDTH, STAGE_LENGTH = 800, 800
 game_running = True
 open_canvas(800, 800)
 
-class Character:
-    def __init__(self, x, y, angle):
-        self.x, self.y, self.angle = x, y, angle
+class Cursur:
+    def __init__(self):
+        pass
+    pass
 
-        self.walk_speed, self.run_speed = 1.0, 2.0
+class Character:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+
+        self.walk_speed, self.run_speed = 2.0, 5.0
 
         # 0 = idle, 1 = walk, 2 = run, 3 = left_strafe, 4 = right_strafe
         self.feet_image = (load_image('image/character/feet/idle.png'),             # cnt = 1
@@ -22,6 +28,7 @@ class Character:
         self.feet_image_height = (self.feet_image[0].h, self.feet_image[1].h, self.feet_image[2].h, self.feet_image[3].h, self.feet_image[4].h)
         self.feet_status = 0
         self.feet_frame = 0
+        self.feet_direction = 0
 
         # 0 = idle, 1 = move, 2 = reload, 3 = shoot
         self.body_image = (load_image('image/character/body/idle.png'),             # cnt = 20
@@ -32,7 +39,7 @@ class Character:
         self.body_image_height = (self.body_image[0].h, self.body_image[1].h, self.body_image[2].h, self.body_image[3].h)
         self.body_status = 0
         self.body_frame = 0
-
+        self.body_rad = 0
     def animation(self):
         # feet animation
         if self.feet_status == 0:
@@ -45,46 +52,128 @@ class Character:
         else:
             self.body_frame = (self.body_frame + 1) % 20
 
-    def move(self):
-        self.feet_status = 1
-        self.feet_frame = 0
-        self.body_status = 1
-        self.body_frame = 0
+    def walk(self, dir):
+        self.feet_status = 1        # 0 = idle, 1 = walk, 2 = run, 3 = left_strafe, 4 = right_strafe
+        self.body_status = 1        # 0 = idle, 1 = move, 2 = reload, 3 = shoot
+        self.feet_direction = dir   # 0 = right, 1 = ru, 2 = up, 3 = lu, 4 = left, 5 = ld, 6 = down, 7 = rd
+
+    def run(self, dir):
+        self.feet_status = 2        # 0 = idle, 1 = walk, 2 = run, 3 = left_strafe, 4 = right_strafe
+        self.body_status = 1        # 0 = idle, 1 = move, 2 = reload, 3 = shoot
+        self.feet_direction = dir   # 0 = right, 1 = ru, 2 = up, 3 = lu, 4 = left, 5 = ld, 6 = down, 7 = rd
 
     def reload(self):
-        self.body_status = 2
-        self.body_frame = 0
+        self.body_status = 2        # 0 = idle, 1 = move, 2 = reload, 3 = shoot
 
     def attack(self):
-        self.body_status = 3
-        self.body_frame = 0
+        self.body_status = 3        # 0 = idle, 1 = move, 2 = reload, 3 = shoot
 
     def idle(self):
-        self.feet_status = 0
+        self.feet_status = 0        # 0 = idle, 1 = walk, 2 = run, 3 = left_strafe, 4 = right_strafe
         self.feet_frame = 0
-        self.body_status = 0
+        self.body_status = 0        # 0 = idle, 1 = move, 2 = reload, 3 = shoot
         self.body_frame = 0
+
+    def update(self):
+        s = 0
+        if self.feet_status == 1:
+            s = self.walk_speed
+        elif self.feet_status == 2:
+            s = self.run_speed
+
+        # 0 = right, 1 = ru, 2 = up, 3 = lu, 4 = left, 5 = ld, 6 = down, 7 = rd
+        move_distance = ((s, 0), (s / 1.4, s / 1.4), (0, s), (-s / 1.4, s / 1.4),
+                         (-s, 0), (-s / 1.4, -s / 1.4), (0, -s), (s / 1.4, -s / 1.4))
+        self.x += move_distance[self.feet_direction][0]
+        self.y += move_distance[self.feet_direction][1]
+
+        self.animation()
 
     def render(self):
         fs, bs = self.feet_status, self.body_status
-        self.feet_image[fs].clip_draw(self.feet_frame * self.feet_image_width[fs], 0, self.feet_image_width[fs], self.feet_image_height[fs],
-                                                    self.x, self.y, self.feet_image_width[fs] // 5, self.feet_image_height[fs] // 5)
-        self.body_image[bs].clip_draw(self.body_frame * self.body_image_width[bs], 0, self.body_image_width[bs], self.body_image_height[bs],
-                                                    self.x, self.y, self.body_image_width[bs] // 5, self.body_image_height[bs] // 5)
+        self.feet_image[fs].clip_composite_draw(self.feet_frame * self.feet_image_width[fs], 0,
+                                                self.feet_image_width[fs], self.feet_image_height[fs],
+                                                (math.pi / 4) * self.feet_direction, '0', self.x, self.y,
+                                                self.feet_image_width[fs] // 5, self.feet_image_height[fs] // 5)
+
+        self.body_image[bs].clip_composite_draw(self.body_frame * self.body_image_width[bs], 0,
+                                                self.body_image_width[bs], self.body_image_height[bs],
+                                                self.body_rad, '0', self.x, self.y,
+                                                self.body_image_width[bs] // 5, self.body_image_height[bs] // 5)
 
 
-player = Character(STAGE_WIDTH // 2, STAGE_LENGTH // 2, 0)
-#player.move()
+
+stage = load_image('image/stage/stage_test.png')
+player = Character(STAGE_WIDTH // 2, STAGE_LENGTH // 2)
+
+
+w, a, s, d = False, False, False, False
+def input_manager():
+    global game_running
+    global w, a, s, d
+    events = get_events()
+    for event in events:
+        if event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                game_running = False
+            if event.key == SDLK_d:
+                d = True
+            elif event.key == SDLK_w:
+                w = True
+            elif event.key == SDLK_a:
+                a = True
+            elif event.key == SDLK_s:
+                s = True
+
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_d:
+                d = False
+            elif event.key == SDLK_w:
+                w = False
+            elif event.key == SDLK_a:
+                a = False
+            elif event.key == SDLK_s:
+                s = False
+
+    if w and s:
+        w, s = False, False
+    if a and d:
+        a, d = False, False
+
+    if w and d:
+        player.walk(1)
+    elif w and a:
+        player.walk(3)
+    elif s and a:
+        player.walk(5)
+    elif s and d:
+        player.walk(7)
+    elif d:
+        player.walk(0)
+    elif w:
+        player.walk(2)
+    elif a:
+        player.walk(4)
+    elif s:
+        player.walk(6)
+    else:
+        player.idle()
 
 def render_scene():
     clear_canvas()
+    stage.draw(STAGE_WIDTH // 2, STAGE_LENGTH // 2, STAGE_WIDTH, STAGE_LENGTH)
     player.render()
     update_canvas()
 
 def game_update():
-    player.animation()
+    input_manager()
+    player.update()
     render_scene()
-    delay(0.05)
+    delay(0.03)
+
+def events_handler():
+    events = get_events()
+    print(events)
 
 while game_running == True:
     game_update()
