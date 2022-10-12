7334b0ae1a79a45ee7be9a4ea09143be1521a18d
jinyoungKim2021182008 <wlsdudkim9693@gmail.com>
2022-10-12 14:54:54 +0900

    object 객체 추가

--- object.py
+++ object.py
@@ -1,3 +1,39 @@
 from pico2d import *
 
+class SandBarricade:
+    def __init__(self, x, y, rad):
+        self.image = load_image('image/stage/stage_object/sand_barricade.png')
+        self.rad = rad
+        self.y = y
+        self.x = x
+    def setpos(self, x, y, rad):
+        self.rad = rad
+        self.x = y
+        self.y = x
 
+    def render(self):
+        self.image.clip_composite_draw(0, 0, self.image.w, self.image.h, self.rad, '0', self.x, self.y, 100, 15)
+
+class SteelBarricade:
+    def __init__(self):
+
+        pass
+
+    def render(self):
+
+        pass
+
+
+class Stone:
+    def __init__(self):
+        pass
+
+    def render(self):
+        pass
+
+class TargetPoint:
+    def __init__(self):
+        pass
+
+    def render(self):
+        pass
