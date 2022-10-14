5d93f5c15116528a871685aa89c07a2e4c107f1d
jinyoungKim2021182008 <wlsdudkim9693@gmail.com>
2022-10-14 22:01:37 +0900

    constants_and_func
    crashCharacter2Object(character, object) 버그 픽스

--- game_constant.py
+++ game_constant.py
@@ -1,15 +1,14 @@
 import math
-
+import object
+import character
+from pico2d import *
 SCENE_WIDTH, SCENE_HEIGHT = 800, 800
 
 CHARACTER_WIDTH, CHARACTER_HEIGHT = 50, 50
-
-
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-
 
 def bullet_crash(bullet, other):
     if type(other).__name__ == 'SandBarricade':
@@ -18,6 +17,47 @@ def bullet_crash(bullet, other):
 
         if crashLine2Rect(bx1, by1, bx2, by2, other.x, other.y, other.rad, 100, 15):
             return True
+
+    return False
+
+def crashCharacter2Object(character, object):
+    d = (object.width / 2) - 20
+    dis = object.height + 10
+    rad = object.rad
+    rx, ry = math.cos(rad), math.sin(rad)
+    x1, y1 = object.x + d * rx, object.y + d * ry
+    x2, y2 = object.x - d * rx, object.y - d * ry
+    if crashCircle2Line(character.x, character.y, dis, x1, y1, x2, y2, rad):
+        
+
+
+def crashCircle2Line(cx, cy, r, x1, y1, x2, y2, rad):
+    """p = (pow(x1 - y1, 2) + pow(x2 - y2, 2))
+    if p == 0:
+        lt = 0
+    else:
+        lt = (x1*y1 - y1*cx - x1*x1 + x1*cx + x2*y2 - y2*cy + x2*cy - x2*x2) / -p
+
+
+    if 0 < lt < 1:
+        lx = (1 - lt) * x1 + lt * x2
+        ly = (1 - lt) * y1 + lt * y2
+        val = pow(lx - cx, 2) + pow(ly - cy, 2)
+        if val <= r * r:
+            return True
+    else:"""
+    drad = rad + math.pi / 2
+    rx, ry = math.cos(drad), math.sin(drad)
+    cx1, cy1 = cx + r * rx, cy + r * ry
+    cx2, cy2 = cx - r * rx, cy - r * ry
+    if crashLine2Line(Point(cx1, cy1), Point(cx2, cy2), Point(x1, y1), Point(x2, y2)):
+        return True
+    val = pow(x1 - cx, 2) + pow(y1 - cy, 2)
+    if val <= r * r:
+        return True
+    val = pow(x2 - cx, 2) + pow(y2 - cy, 2)
+    if val <= r * r:
+        return True
 
     return False
 
